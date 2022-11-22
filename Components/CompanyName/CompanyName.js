f8b24d8cc45b84175b0efffa6b2a0f36e1e4cedd
tanvir <tanvirulislam149@gmail.com>
2022-11-22 15:08:28 +0600

    companyName done

--- Components/CompanyName/CompanyName.js
+++ Components/CompanyName/CompanyName.js
@@ -5,10 +5,11 @@ import netflix from "../../images/logos/netflix.png"
 import slack from "../../images/logos/slack.png"
 import uber from "../../images/logos/uber.png"
 import Image from 'next/image'
+import styles from "./CompanyName.module.css"
 
 const CompanyName = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Image
         src={google}
         alt="Picture of the author"
@@ -18,26 +19,26 @@ const CompanyName = () => {
       <Image
         src={airbnb}
         alt="Picture of the author"
-        width={130}
-        height={45}
+        width={160}
+        height={51}
       />
       <Image
         src={netflix}
         alt="Picture of the author"
-        width={130}
-        height={45}
+        width={120}
+        height={67}
       />
       <Image
         src={slack}
         alt="Picture of the author"
-        width={130}
-        height={45}
+        width={140}
+        height={36}
       />
       <Image
         src={uber}
         alt="Picture of the author"
-        width={130}
-        height={45}
+        width={99}
+        height={56}
       />
     </div>
   )
