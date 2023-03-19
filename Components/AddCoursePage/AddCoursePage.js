3c295a27d8de2ff261b54e6517418f1a6e6d1ea1
tanvir <tanvirulislam149@gmail.com>
2023-03-19 23:41:49 +0600

    add course ui done

--- Components/AddCoursePage/AddCoursePage.js
+++ Components/AddCoursePage/AddCoursePage.js
@@ -5,7 +5,8 @@ import Typography from '@mui/material/Typography';
 import styles from "./AddCoursePage.module.css"
 import SideNav from '../SideNav/SideNav';
 import { useForm } from "react-hook-form";
-import { Alert } from '@mui/material';
+
+
 
 
 const drawerWidth = 200;
@@ -24,41 +25,47 @@ const AddCoursePage = () => {
       >
         <Toolbar />
         <div>
-          <h1>Add Course</h1>
+          <p className={styles.addCourse}>Add Course</p>
           <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name">Name:</label>
-            <input {...register("name", { required: true, })}
-              aria-invalid={errors.name ? "true" : "false"} /> <br />
-            {errors.name?.type === 'required' && <Alert severity="error">This is an error alert — check it out!</Alert>}
-            <label htmlFor="name_details">Name in Details:</label>
-            <input {...register("name_details", { required: true, })} /> <br />
-            <label htmlFor="short_des">Short Description:</label>
-            <input {...register("short_des", { required: true, })} /> <br />
-            <label htmlFor="short_details">Short Details:</label>
-            <input {...register("short_details", { required: true, })} /> <br />
-            <label htmlFor="details">Details:</label>
-            <input {...register("details", { required: true, })} /> <br />
-            <label htmlFor="requirements">Requirements:</label>
-            <input {...register("requirements", { required: true, })} /> <br />
-            <label htmlFor="language">Language:</label>
-            <input {...register("language", { required: true, })} /> <br />
-            <label htmlFor="author">Author:</label>
-            <input {...register("author", { required: true, })} /> <br />
-            <label htmlFor="duration">Duration:</label>
-            <input type="number" {...register("duration", { required: true })} /> <br />
-            <label htmlFor="project">Project:</label>
-            <input type="number" {...register("project", { required: true })} /> <br />
-            <label htmlFor="price">Price:</label>
-            <input type="number" {...register("price", { required: true })} /> <br />
-            <label htmlFor="students">Students:</label>
-            <input type="number" {...register("students", { required: true })} /> <br />
-            <label htmlFor="details_img">Image for Details:</label>
-            <input {...register("details_img", { required: true })} /> <br />
-            <label htmlFor="picture">Picture:</label>
-            <input {...register("picture", { required: true })} /> <br />
-            <label htmlFor="video_link">Video Link:</label>
-            <input {...register("video_link", { required: true, })} /> <br />
-            <input type="submit" />
+            <div className={styles.formContainer}>
+              <div className={styles.leftColumn}>
+                <label htmlFor="name">Title</label><br />
+                <input {...register("name", { required: true, })} /> <br />
+                <label htmlFor="name_details">Course Name</label><br />
+                <input {...register("name_details", { required: true, })} /> <br />
+                <label htmlFor="short_des">Short Description</label><br />
+                <textarea {...register("short_des", { required: true })} /> <br />
+                <label htmlFor="short_details">Short Details</label><br />
+                <textarea {...register("short_details", { required: true })} /> <br />
+                <label htmlFor="details">Details</label><br />
+                <textarea {...register("details", { required: true })} /> <br />
+                <label htmlFor="requirements">Requirements</label><br />
+                <textarea {...register("requirements", { required: true })} /> <br />
+                <label htmlFor="language">Language</label><br />
+                <input {...register("language", { required: true, })} /> <br />
+              </div>
+              <div className={styles.leftColumn}>
+                <label htmlFor="author">Author</label> <br />
+                <input {...register("author", { required: true, })} /> <br />
+                <label htmlFor="duration">Duration</label><br />
+                <input type="number" {...register("duration", { required: true })} /> <br />
+                <label htmlFor="project">Project</label><br />
+                <input type="number" {...register("project", { required: true })} /> <br />
+                <label htmlFor="price">Price</label><br />
+                <input type="number" {...register("price", { required: true })} /> <br />
+                <label htmlFor="students">Students</label><br />
+                <input type="number" {...register("students", { required: true })} /> <br />
+                <label htmlFor="details_img">Image for Details</label><br />
+                <input {...register("details_img", { required: true })} /> <br />
+                <label htmlFor="picture">Picture</label><br />
+                <input {...register("picture", { required: true })} /> <br />
+                <label htmlFor="video_link">Video Link</label><br />
+                <input {...register("video_link", { required: true, })} /> <br />
+              </div>
+            </div>
+            <div className={styles.btnContainer}>
+              <input className={styles.submitBtn} type="submit" />
+            </div>
           </form>
         </div>
       </Box>
