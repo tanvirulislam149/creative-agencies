449f07edc88961bb6533fde67f9ff642f1dfbaf2
tanvir <tanvirulislam149@gmail.com>
2023-03-25 23:10:41 +0600

    add course page

--- Components/AddCoursePage/AddCoursePage.js
+++ Components/AddCoursePage/AddCoursePage.js
@@ -5,15 +5,37 @@ import Typography from '@mui/material/Typography';
 import styles from "./AddCoursePage.module.css"
 import SideNav from '../SideNav/SideNav';
 import { useForm } from "react-hook-form";
-
-
-
+import axios from 'axios';
 
 const drawerWidth = 200;
 
 const AddCoursePage = () => {
-  const { register, formState: { errors }, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, formState: { errors }, handleSubmit, reset } = useForm();
+  const [detailsField, setDetailsField] = React.useState([""]);
+  const [requirementField, setRequirementField] = React.useState([""]);
+
+  const onSubmit = data => {
+    console.log(data)
+    // axios.post(`http://localhost:5000/addProduct`, data)
+    //   .then(res => {
+    //     // handle success
+    //     console.log(res.data);
+    //   })
+    //   .catch(err => {
+    //     // handle error
+    //     console.log(err);
+    //   })
+    // reset();
+  };
+
+  const handleDetailsField = () => {
+    setDetailsField([...detailsField, ""])
+
+  }
+
+  const handleRequirementField = () => {
+    setRequirementField([...requirementField, ""])
+  }
 
   return (
     <Box className={styles.boxContainer} sx={{ display: 'flex' }}>
@@ -30,37 +52,48 @@ const AddCoursePage = () => {
             <div className={styles.formContainer}>
               <div className={styles.leftColumn}>
                 <label htmlFor="title">Title</label><br />
-                <input {...register("title", { required: true, })} /> <br />
+                <input placeholder='Enter title' {...register("title", { required: true, })} /> <br />
                 <label htmlFor="course_name">Course Name</label><br />
-                <input {...register("course_name", { required: true, })} /> <br />
+                <input placeholder='Enter Course Name' {...register("course_name", { required: true, })} /> <br />
                 <label htmlFor="description">Description</label><br />
-                <textarea {...register("description", { required: true })} /> <br />
+                <textarea placeholder='Enter Description' {...register("description", { required: true })} /> <br />
                 <label htmlFor="short_details">Short Details</label><br />
-                <textarea {...register("short_details", { required: true })} /> <br />
-                <label htmlFor="details">Details</label><br />
-                <textarea {...register("details", { required: true })} /> <br />
-                <label htmlFor="requirements">Requirements</label><br />
-                <textarea {...register("requirements", { required: true })} /> <br />
-                <label htmlFor="language">Language</label><br />
-                <input {...register("language", { required: true, })} /> <br />
+                <textarea placeholder='Enter Short Details' {...register("short_details", { required: true })} /> <br />
+                <div className={styles.details}>
+                  <label htmlFor="details">Details</label>
+                  <input type="button" onClick={handleDetailsField} className={styles.addField} value="Add Field" />
+                </div>
+                {
+                  detailsField.map(d => <><textarea placeholder='Enter Details' {...register("details", { required: true })} /> <br /></>)
+                }
+                <div className={styles.details}>
+                  <label htmlFor="requirement">Requirements</label>
+                  <input type="button" onClick={handleRequirementField} className={styles.addField} value="Add Field" />
+                </div>
+                {
+                  requirementField.map(d => <><textarea placeholder='Enter Requirements' {...register("requirements", { required: true })} /> <br /></>)
+                }
               </div>
               <div className={styles.leftColumn}>
+
+                <label htmlFor="language">Language</label><br />
+                <input placeholder='Enter Language' {...register("language", { required: true, })} /> <br />
                 <label htmlFor="author">Author</label> <br />
-                <input {...register("author", { required: true, })} /> <br />
+                <input placeholder="Enter Author's Name" {...register("author", { required: true, })} /> <br />
                 <label htmlFor="duration">Duration</label><br />
-                <input type="number" {...register("duration", { required: true })} /> <br />
+                <input placeholder='Enter Course Duration' type="number" {...register("duration", { required: true })} /> <br />
                 <label htmlFor="project">Project</label><br />
-                <input type="number" {...register("project", { required: true })} /> <br />
+                <input placeholder='Enter Project Number' type="number" {...register("project", { required: true })} /> <br />
                 <label htmlFor="price">Price</label><br />
-                <input type="number" {...register("price", { required: true })} /> <br />
+                <input placeholder='Enter Price' type="number" {...register("price", { required: true })} /> <br />
                 <label htmlFor="students">Students</label><br />
-                <input type="number" {...register("students", { required: true })} /> <br />
+                <input placeholder='Enter Student Number' type="number" {...register("students", { required: true })} /> <br />
                 <label htmlFor="picture">Picture</label><br />
-                <input {...register("picture", { required: true })} /> <br />
+                <input placeholder='Enter Picture Link' {...register("picture", { required: true })} /> <br />
                 <label htmlFor="icon">Icon</label><br />
-                <input {...register("icon", { required: true })} /> <br />
+                <input placeholder='Enter Icon Link' {...register("icon", { required: true })} /> <br />
                 <label htmlFor="video_link">Video Link</label><br />
-                <input {...register("video_link", { required: true, })} /> <br />
+                <input placeholder='Enter Video Link' {...register("video_link", { required: true, })} /> <br />
               </div>
             </div>
             <div className={styles.btnContainer}>
