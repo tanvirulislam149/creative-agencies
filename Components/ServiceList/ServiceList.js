103ca6d2fd8bee9c654a9a25d5f2acd0029aa753
tanvir <tanvirulislam149@gmail.com>
2023-07-26 12:16:26 +0600

    all orderlist ui done

--- Components/ServiceList/ServiceList.js
+++ Components/ServiceList/ServiceList.js
@@ -1,4 +1,4 @@
-import { Toolbar, Typography } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select, Toolbar, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react'
 import SideNav from '../SideNav/SideNav';
@@ -7,6 +7,13 @@ import styles from "./ServiceList.module.css"
 const drawerWidth = 200;
 
 const ServiceList = () => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  console.log(age);
+
   return (
     <Box className={styles.boxContainer} sx={{ display: 'flex' }}>
       <Box
@@ -23,7 +30,7 @@ const ServiceList = () => {
                 <th className={`${styles.tableHeader} ${styles.firstHeader}`}>Name</th>
                 <th className={styles.tableHeader}>Email Id</th>
                 <th className={styles.tableHeader}>Service</th>
-                <th className={styles.tableHeader}>Project Details</th>
+                <th className={`${styles.tableHeader} ${styles.detailsHeader}`}>Project Details</th>
                 <th className={`${styles.tableHeader} ${styles.lastHeader}`}>Status</th>
               </tr>
             </thead>
@@ -32,29 +39,97 @@ const ServiceList = () => {
                 <td>Alfreds Futterkiste</td>
                 <td>Maria Anders</td>
                 <td>Germany</td>
-                <td>Germany</td>
-                <td>Germany</td>
+                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, nostrum?</td>
+                <td>
+                  <FormControl variant="standard" sx={{ minWidth: 120, backgroundColor: "#f5f6fa", paddingLeft: "5px" }}>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={age}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      {/* <MenuItem value="">
+                        <em>Select</em>
+                      </MenuItem> */}
+                      <MenuItem value={10}>Pending</MenuItem>
+                      <MenuItem value={20}>Done</MenuItem>
+                      <MenuItem value={30}>On Going</MenuItem>
+                    </Select>
+                  </FormControl>
+                </td>
               </tr>
               <tr>
                 <td>Centro comercial Moctezuma</td>
                 <td>Francisco Chang</td>
                 <td>Mexico</td>
                 <td>Mexico</td>
-                <td>Mexico</td>
+                <td>
+                  <FormControl variant="standard" sx={{ minWidth: 120, backgroundColor: "#f5f6fa", paddingLeft: "5px" }}>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={age}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      {/* <MenuItem value="">
+                        <em>Select</em>
+                      </MenuItem> */}
+                      <MenuItem value={10}>Pending</MenuItem>
+                      <MenuItem value={20}>Done</MenuItem>
+                      <MenuItem value={30}>On Going</MenuItem>
+                    </Select>
+                  </FormControl>
+                </td>
               </tr>
               <tr>
                 <td>Ernst Handel</td>
                 <td>Roland Mendel</td>
                 <td>Austria</td>
-                <td>Austria</td>
-                <td>Austria</td>
+                <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, labore.</td>
+                <td>
+                  <FormControl variant="standard" sx={{ minWidth: 120, backgroundColor: "#f5f6fa", paddingLeft: "5px" }}>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={age}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      {/* <MenuItem value="">
+                        <em>Select</em>
+                      </MenuItem> */}
+                      <MenuItem value={10}>Pending</MenuItem>
+                      <MenuItem value={20}>Done</MenuItem>
+                      <MenuItem value={30}>On Going</MenuItem>
+                    </Select>
+                  </FormControl>
+                </td>
               </tr>
               <tr>
                 <td>Island Trading</td>
                 <td>Helen Bennett</td>
                 <td>Helen Bennett</td>
                 <td>Helen Bennett</td>
-                <td>UK</td>
+                <td>
+                  <FormControl variant="standard" sx={{ minWidth: 120, backgroundColor: "#f5f6fa", paddingLeft: "5px" }}>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={age}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      {/* <MenuItem value="">
+                        <em>Select</em>
+                      </MenuItem> */}
+                      <MenuItem value={10}>Pending</MenuItem>
+                      <MenuItem value={20}>Done</MenuItem>
+                      <MenuItem value={30}>On Going</MenuItem>
+                    </Select>
+                  </FormControl>
+                </td>
               </tr>
               <tr>
                 <td>Laughing Bacchus Winecellars</td>
