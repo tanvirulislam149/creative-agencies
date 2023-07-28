d15ada365b52155b1dcf611343068588156c670c
tanvir <tanvirulislam149@gmail.com>
2023-07-28 23:04:30 +0600

    refetch done

--- Redux/Services/courses.js
+++ Redux/Services/courses.js
@@ -3,16 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const getCoursesApi = createApi({
   reducerPath: "getCourses",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/course" }),
+  tagTypes: ["Course"],
   endpoints: (builder) => ({
     getCourses: builder.query({
       query: () => "/getCourses",
+      providesTags: ['Course'],
     }),
     addCourse: builder.mutation({
       query: (data) => ({
         url: "/addCourse",
         method: "POST",
         body: data
-      })
+      }),
+      invalidatesTags: ['Course'],
     })
   })
 })
