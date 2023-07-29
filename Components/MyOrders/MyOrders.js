0bded9703d73a78536f0b5d6c5e7fe66a45d59c5
tanvir <tanvirulislam149@gmail.com>
2023-07-29 14:39:23 +0600

    servicelist ui done

--- Components/MyOrders/MyOrders.js
+++ Components/MyOrders/MyOrders.js
@@ -11,10 +11,42 @@ const MyOrders = () => {
       <Box
         className={styles.boxContent}
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{ flexGrow: 1, p: 0, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
       >
         <div>
-          <h1 className={styles.myOrders}>Service List</h1>
+          <p className={styles.myOrders}>Service List</p>
+          <div className={styles.orderCardCont}>
+            <div className={styles.card}>
+              <div className={styles.imgCont}>
+                <img className={styles.icon} src="https://i.ibb.co/D7msbHw/aboutme.jpg" alt="" />
+                <div className={styles.onGoingStatus}>
+                  <p>On going</p>
+                </div>
+              </div>
+              <p className={styles.title}>Web & Mobile design</p>
+              <p className={styles.description}>We craft stunning and amazing web UI, using a well drrafted UX to fit your product.</p>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.imgCont}>
+                <img className={styles.icon} src="https://i.ibb.co/D7msbHw/aboutme.jpg" alt="" />
+                <div className={styles.pendingStatus}>
+                  <p>Pending</p>
+                </div>
+              </div>
+              <p className={styles.title}>Web & Mobile design</p>
+              <p className={styles.description}>We craft stunning and amazing web UI, using a well drrafted UX to fit your product.</p>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.imgCont}>
+                <img className={styles.icon} src="https://i.ibb.co/D7msbHw/aboutme.jpg" alt="" />
+                <div className={styles.doneStatus}>
+                  <p>Done</p>
+                </div>
+              </div>
+              <p className={styles.title}>Web & Mobile design</p>
+              <p className={styles.description}>We craft stunning and amazing web UI, using a well drrafted UX to fit your product.</p>
+            </div>
+          </div>
         </div>
       </Box>
     </Box>
