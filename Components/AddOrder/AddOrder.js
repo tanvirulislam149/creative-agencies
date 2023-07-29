5fd9d7e68aeb0347923030d9c5136ed0ca5bb9f3
tanvir <tanvirulislam149@gmail.com>
2023-07-29 13:50:58 +0600

    addOrder UI done

--- Components/AddOrder/AddOrder.js
+++ Components/AddOrder/AddOrder.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import styles from "./AddOrder.module.css"
-import { Box, Toolbar } from '@mui/material'
+import { Box, FormControl, MenuItem, Select, Toolbar } from '@mui/material'
 import { useForm } from 'react-hook-form';
 
 const drawerWidth = 200;
 
 const AddOrder = () => {
   const { register, formState: { errors }, handleSubmit, reset } = useForm();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
 
   const onSubmit = (data) => {
-    console.log(data);
+    const finalData = { ...data, service: age }
+    console.log(finalData);
   }
 
   return (
@@ -27,11 +33,26 @@ const AddOrder = () => {
               <div className={styles.leftColumn}>
                 <input type='text' placeholder="Your name / company's name" {...register("name", { required: true, })} /> <br />
                 <input type='email' placeholder="Your email address" {...register("email", { required: true, })} /> <br />
-                <input type='text' placeholder="Service you want" {...register("service", { required: true, })} /> <br />
+                <FormControl sx={{ marginBottom: "10px", marginTop: "5px", width: "60%", background: "white" }}>
+                  <Select
+                    required
+                    value={age}
+                    onChange={handleChange}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                  >
+                    <MenuItem value="">
+                      <span className={styles.selectPlaceholder}>Service you want</span>
+                    </MenuItem>
+                    <MenuItem value={10}><span className={styles.selectOption}>Your email address</span></MenuItem>
+                    <MenuItem value={20}><span className={styles.selectOption}>email address</span></MenuItem>
+                    <MenuItem value={30}><span className={styles.selectOption}>Your email</span></MenuItem>
+                  </Select>
+                </FormControl> <br />
                 <textarea rows={5} placeholder='Project Details' {...register("description", { required: true, })} /> <br />
               </div>
               <div className={styles.priceCont}>
-                <input type='number' placeholder='Price' {...register("price", { required: true, })} /> <br />
+                <input type='Number' placeholder='Price' {...register("price", { required: true, })} /> <br />
                 <label className={styles.picture}>
                   <img src="https://cdn-icons-png.flaticon.com/512/126/126477.png" />
                   <p>Upload project image</p>
