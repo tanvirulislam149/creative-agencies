5c02b1ae1315ea07a9e84e71359ed8a7cc699af7
tanvir <tanvirulislam149@gmail.com>
2023-08-08 19:31:52 +0600

    admin making fixed

--- pages/dashboard/adminMaking/index.js
+++ pages/dashboard/adminMaking/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import MakeAdminPage from '../../../Components/adminMakingPage/adminMakingPage'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useRouter } from 'next/router';
@@ -9,6 +8,7 @@ import PrivateUserRoute from '../../../Layouts/PrivateUserRoute';
 import PrivateAdminRoute from '../../../Layouts/PrivateAdminRoute';
 import DashboardLayout from '../../../Layouts/DashboardLayout';
 import NavbarLayout from '../../../Layouts/NavbarLayout';
+import MakeAdminPage from '../../../Components/MakeAdminPage/MakeAdminPage';
 
 const MakeAdmin = () => {
   return (
